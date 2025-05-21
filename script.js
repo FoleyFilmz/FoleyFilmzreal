a42da745e6bab09fc98fc471a64a9800d35a041d
FoleyFilmz <dylanharlenfoley@gmail.com>
2025-05-21 17:00:54 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function showVideos() {
-  document.querySelector('.event-card').classList.add('hidden');
-  document.getElementById('videos').classList.remove('hidden');
-}
-function goBack() {
-  document.getElementById('videos').classList.add('hidden');
-  document.querySelector('.event-card').classList.remove('hidden');
-}

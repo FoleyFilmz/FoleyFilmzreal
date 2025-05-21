b8bb52c4797bc774befc7252a650d71fdeb5777a
FoleyFilmz <dylanharlenfoley@gmail.com>
2025-05-21 17:21:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const app = document.getElementById("app");
+  app.innerHTML = '<h1>FoleyFilmZ FULLSITE Loaded</h1><p>Admin & rider system is in place.</p>';
+});

13f0fb4f9c3dd63db7f9542efbdfae9e509e9efd
FoleyFilmz <dylanharlenfoley@gmail.com>
2025-05-22 11:40:15 -0400

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const app = document.getElementById("app");
-  app.innerHTML = '<h1>FoleyFilmZ FULLSITE Loaded</h1><p>Admin & rider system is in place.</p>';
-});
